4738730e3e0867d84fa6dec37ec1361f129567da
Andrew <121936658+7gxycn08@users.noreply.github.com>
2024-11-13 13:40:46 +0400

    Updated to v1.8

--- ClassicMixer.py
+++ ClassicMixer.py
@@ -1,27 +1,55 @@
+import ctypes
 import subprocess
-import sys
+import pygetwindow as gw
+import pyautogui
 import threading
 import time
-import configparser
-import pyautogui
-from PyQt5.QtWidgets import QMenu, QSystemTrayIcon,QApplication
-from PyQt5.QtGui import QIcon
 from pynput import mouse
+import configparser
+import sys
+from PySide6.QtWidgets import QMenu, QSystemTrayIcon, QApplication
+from PySide6.QtGui import QIcon, QAction
+from PySide6.QtCore import Qt
 
+# Constants for window position flags
+SWP_NOACTIVATE = 0x0010
+SWP_NOSIZE = 0x0001
+SWP_NOZORDER = 0x0004
+SWP_SHOWWINDOW = 0x0040
+screen_width, screen_height = pyautogui.size()
+window_name = "volume mixer"
 config = configparser.ConfigParser()
 config.read('Config.ini')
 movable = str(config['MainConfig']['moveable'])
 flag = True
 
-def Tray_Icon():
-    global process
+def tray_icon():
     def close_tray_icon():
-        tray_icon.hide()
+        classic_tray.hide()
         app.exit()
         sys.exit()
 
+    def on_double_click(reason):
+        global flag
+        if reason == QSystemTrayIcon.ActivationReason.Trigger:
+            threading.Thread(target=lambda: launch_and_move_window("sndvol.exe"),daemon=True).start()
+            subprocess.Popen(r"sndvol.exe", creationflags=subprocess.CREATE_NO_WINDOW)
+            flag = True
+
+    # Function to set the window position immediately
+    def move_window(hwnd, x, y, width, height):
+        ctypes.windll.user32.SetWindowPos(hwnd, 0, x, y, width, height,
+                                          SWP_NOACTIVATE | SWP_NOSIZE | SWP_SHOWWINDOW | SWP_NOZORDER)
+
+    # Function to find the window handle (HWND) by process name
+    def find_window_by_process():
+        windows = gw.getWindowsWithTitle(window_name)
+        if windows:
+            return windows[0]._hWnd
+        return None
+
     def on_click(x, y, x_min, y_min, x_max, y_max, button, pressed):
-        global flag,movable
+        global flag, movable
         if movable == "True":
             flag = False
             return
@@ -41,46 +69,56 @@ def Tray_Icon():
         with mouse.Listener(on_click=lambda x, y, button, pressed: on_click(x, y, x_min, y_min, x_max, y_max, button,
                                                                             pressed)) as listener:
             while flag:
-                time.sleep(0.1)
+                time.sleep(1)
                 continue
 
-    def move_window_to_bottom_right(process_name):
-        while True:
-            try:
-                window = pyautogui.getWindowsWithTitle(process_name)
-                if window:
-                    window = window[0]
-                    # do something with the window
-                    screen_width, screen_height = pyautogui.size()
-                    x_min, y_min = screen_width - window.width, screen_height - window.height
-                    x_max, y_max = screen_width, screen_height
-                    x_min, y_min = x_min - 80, y_min - 80
-                    threading.Thread(target=lambda :start_mouse_listener(x_min, y_min, x_max, y_max),
-                                     daemon=True).start()
-                    break
-            except:
-                pass
+    # Function to launch and move the window to the bottom-right corner
+    def launch_and_move_window(process_name):
+        # Launch the process
+        subprocess.Popen([process_name])
 
-    def onDoubleClick(reason):
-        global flag
-        if reason == QSystemTrayIcon.Trigger:
-            process_name = "volume mixer"
-            threading.Thread(target=lambda: move_window_to_bottom_right(process_name),daemon=True).start()
-            subprocess.Popen(r"bin\classicmixerbin.exe",creationflags=subprocess.CREATE_NO_WINDOW)
-            flag = True
+        retries = 5000  # Retry until window is found
+        hwnd = None
+        for _ in range(retries):
+            hwnd = find_window_by_process()
+            if hwnd:
+                break
+
+        if hwnd:
+            # Define desired position for the bottom-right corner
+            desired_width, desired_height = 1017, 834  # Adjust window size
+            x = screen_width - desired_width - 30  # padding from the right edge
+            y = screen_height - desired_height - 110  # padding from the bottom edge
+            x_max = x + desired_width
+            y_max = y + desired_height
+            # Move the window immediately to the bottom-right corner
+            move_window(hwnd, x, y, desired_width, desired_height)
+            threading.Thread(target=lambda: start_mouse_listener(x, y, x_max, y_max),
+                             daemon=True).start()
+    def about_page():
+        subprocess.Popen("start https://github.com/7gxycn08/ClassicMixer",
+                         shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
     app = QApplication(sys.argv)
-
-    tray_icon = QSystemTrayIcon()
-    tray_icon.setToolTip("Classic Mixer")
-    tray_icon.setIcon(QIcon(f'Dependancy\\Resources\\sound.ico'))
+    classic_tray = QSystemTrayIcon()
+    classic_tray.setToolTip("Classic Mixer")
+    classic_tray.setIcon(QIcon(r'Dependency\Resources\sound.ico'))
 
     menu = QMenu()
-    menu.addAction("Exit").triggered.connect(lambda: close_tray_icon())
-    tray_icon.activated.connect(onDoubleClick)
+    menu.setWindowFlag(Qt.WindowType.WindowStaysOnTopHint, True)
+    menu.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+    menu.setWindowFlags(menu.windowFlags() | Qt.WindowType.FramelessWindowHint)
 
-    tray_icon.setContextMenu(menu)
-    tray_icon.show()
-    app.exec_()
+    about_button = QAction(QIcon(r"Dependency\Resources\about.ico"), 'About')
+    about_button.triggered.connect(about_page)
+    action_exit = QAction(QIcon(r"Dependency\Resources\exit.ico"), 'Exit')
+    action_exit.triggered.connect(close_tray_icon)
+    menu.addActions([about_button, action_exit])
+
+    classic_tray.activated.connect(on_double_click)
+    classic_tray.setContextMenu(menu)
+    classic_tray.show()
+    app.exec()
 
 if __name__ == '__main__':
-    Tray_Icon()
+    tray_icon()
